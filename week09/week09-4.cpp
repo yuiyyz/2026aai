ee1accd80a0e8e97b1f4ae3a860a9abfd1d1e356
yuiyyz <14750580@me.mcu.edu.tw>
2026-04-22 11:50:18 +0800

    week09

---
+++ week09/week09-4.cpp
@@ -0,0 +1,11 @@
+///week09-4.cpp要交換陣列裡的數字
+#include <stdio.h>
+int main()
+{
+    int a=99, b=34;///有兩個水平 a裝咖啡 b裝清水
+    printf("一開始 a: %d b: %d\n", a, b);
+    int temp = a; ///在準備第三個瓶子 temp 先接住a的咖啡
+    a = b;///a瓶子,就可以接住b的清水
+    b = temp;///b瓶子,就可以接住 temp裡暫時放的咖啡
+    printf("交換後a: %d b: %d\n", a, b);
+}
---
+++ week09/week09-6.cpp
@@ -0,0 +1,22 @@
+///week09-6.cpp 重覆 week09-5做很多次 變成「泡泡排序法Bubble Sort」
+#include <stdio.h>
+int main()
+{///陣列有10個數字
+    int a[10] = {11.22,33,44,55,66,77,88,99,97};
+
+    for(int i=0; i<10; i++) printf("%d",a[i]);///擠一行
+    printf("\n");///跳行
+
+    for(int k=0;k<10-1;k++){///只加這一行
+      for(int i=0; i<10-1; i++){///小心, i
+       if(a[i] < a[i+1]) {///希望左大,右小,但現在「反過來」
+          int temp = a[i]; ///就交換
+          a[i] = a[i+1];
+          a[i+1] = temp;
+       }
+     }
+    ///再把陣列印一次,發現最小的 11 移到最右邊
+    for(int i=0;i<10;i++)printf("%d ", a[i]);///擠一行
+    printf("\n");///跳行
+   }///只加這一行
+}
